f0a4dfd5ef88d2df6eb58ffe2b51db02fcdf7ea0
ViolaZhou7 <zypviole@hotmail.com>
2016-10-19 16:49:14 -0700

    Add files via upload

---
+++ crawlNewLinks.py
@@ -0,0 +1,21 @@
+# only links are new should be crawled for additional links
+# looks for all links that begin with /wiki/ (don't restrict to article links)
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html)
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # encountered a new page
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks("")
